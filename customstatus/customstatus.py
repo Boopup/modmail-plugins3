126caaaee06750ac400314775c91773623bf025e
Boo <99917823+Boopup@users.noreply.github.com>
2024-06-28 16:30:47 -0500

    Create customstatus.py

---
+++ customstatus/customstatus.py
@@ -0,0 +1,21 @@
+import discord
+from discord.ext import commands
+
+class CustomActivity(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(name='customactivity')
+    @commands.has_permissions(administrator=True)
+    async def set_custom_activity(self, ctx, emoji: str, *, activity_name: str):
+        activity = discord.CustomActivity(name=activity_name, emoji=emoji)
+        await self.bot.change_presence(activity=activity)
+        await ctx.send(f'Set custom activity to {emoji} {activity_name}')
+
+    @set_custom_activity.error
+    async def custom_activity_error(self, ctx, error):
+        if isinstance(error, commands.MissingPermissions):
+            await ctx.send("You need to be an administrator to use this command.")
+
+def setup(bot):
+    bot.add_cog(CustomActivity(bot))
